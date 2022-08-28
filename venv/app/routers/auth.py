8ac8d11999d43a394098805b0e54914638f4cc9d
charik-goyal <charikgoyal8@gmail.com>
2022-08-28 18:19:55 +0530

    Added the baearer token on Posts routes

--- venv/app/routers/auth.py
+++ venv/app/routers/auth.py
@@ -6,7 +6,7 @@ from .. import database, schemas, models, utils, oauth2
 router = APIRouter(tags=['Authentication'])
 
 
-@router.post('/login')
+@router.post('/login', response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
     user = db.query(models.User).filter(
@@ -23,4 +23,4 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     # create token
     access_token = oauth2.create_access_token(data={"user_id": user.id})
 
-    return {"access token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer"}
